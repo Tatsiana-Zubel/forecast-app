63bb6a4d3d4d10aa7c02088067f38a8b7345dd41
Tatsiana-Zubel <tatsiana.zubel@icloud.com>
2022-10-31 17:28:16 +0100

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <CardComponent title="Current weather"/>
+</template>
+
+<script>
+import CardComponent from './components/CardComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CardComponent
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+#app {
+  font-family: 'Menlo', sans-serif;
+  color: #24292e;
+  background-color: #f5f8fa;
+  height: 100vh;
+  padding: 25px;
+
+}
+</style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <span>{{ title }}</span>
+    <p>
+      Hey,<br>
+      this is card paragraph
+    </p>
+  </div>
+</template>
+
+<script>
+import './CardComponent.scss'
+export default {
+  name: 'CardComponent',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style>
+</style>
